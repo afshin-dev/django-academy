ce381302cd90eb1f3a09fe28a02cea36d32da36f
afshin <shahinof10009@gmail.com>
2022-10-05 01:44:17 +0330

    setting up templates in project and tmpl_hi 'view' for testing

--- playground/urls.py
+++ playground/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from playground.views import hi
+from playground.views import hi, tmpl_hi
 
 urlpatterns = [
     path("hi/", hi, name="playground\hi"),
+    path("tmpl_hi/", tmpl_hi, name="playground\tmpl_hi"),
 ]
--- playground/views.py
+++ playground/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def hi(request):
     from django.http import HttpResponse
     return HttpResponse("hi.")
+
+
+def tmpl_hi(request):
+    return render(request, "playground/hi.html")
